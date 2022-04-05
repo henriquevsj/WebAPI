a61bef2a952164515c96e788fa888c98902b0e03
Henrique Vieira <henrique.v.santos@outlook.com>
2022-04-05 10:24:22 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //
+            optionsBuilder.UseSqlServer(@"Password=dcrrwmp3; TrustServerCertificate=True; User ID=sa;Initial Catalog=HeroApp;Data Source=HUNB1044\SQLSERVER");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
---
+++ WebAPI/Models/IdentidadeSecreta.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public class IdentidadeSecreta
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public int HeroiId { get; set; }
+        public Heroi Heroi { get; set; }
+
+    }
+}
